24cb88bc4a2c2c9334c8f1fc0bda039cca9cc0b4
Korab <korabspahiu123@hotmail.com>
2019-06-02 19:04:33 +0200

    blleh

--- Loja me letra/Start.php
+++ Loja me letra/Start.php
@@ -9,7 +9,6 @@
 <p>This is a simple betting game involving a deck of 54 cards (2 jokers).<br>
 Four cards will be dealt and you will place bets on the qualities of the fifth card (face and suit).<br>
 Payouts for the bets are (roughly) proportional to the probability.</p>
-<p>Creator: Michael Gordon, Adjunct Lecturer, Queens College, CUNY</p>
 <p>You start with $100.</p>
 <?php
 session_start();
@@ -46,16 +45,5 @@ $_SESSION['cash'] = 100;
 <form action="Bet.php" method="post">
 <input type="submit" value="Play!">
 </form>
-<p>
-<b>Future Improvements:</b></p>
-<ul>
-<li>Enhanced appearance with CSS</li>
-<li>Quicker and more detailed betting options</li>
-<li>List of high scores</li>
-<li>Removal of deprecating HTML tags, if any</li>
-</ul>
-</p>
-<p>The next version will probably be in JavaScript</li></p>
-<p>Feedback is welcome. If you're viewing this, you should already have my contact info. :) </p>
 </body>
 </html>
